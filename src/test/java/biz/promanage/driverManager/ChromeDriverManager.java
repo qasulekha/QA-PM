edc88413d1ec915201106d12de61266d8aad4bfd
qasulekha <qa@sulekha.com>
2025-09-10 19:57:43 +0530

    Headless

--- src/test/java/biz/promanage/driverManager/ChromeDriverManager.java
+++ src/test/java/biz/promanage/driverManager/ChromeDriverManager.java
@@ -19,7 +19,7 @@ public class ChromeDriverManager extends DriverManager {
 	@Override
 	protected void startDriver() throws IOException {
 	    ChromeOptions chromeOptions = new ChromeOptions();
-	   // chromeOptions.addArguments("--headless=new");
+	    chromeOptions.addArguments("--headless=new");
 	    chromeOptions.addArguments("--disable-gpu");
 	    chromeOptions.addArguments("--incognito");
 	    chromeOptions.addArguments("start-maximized");
