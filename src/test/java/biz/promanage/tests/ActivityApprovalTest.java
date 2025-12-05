21cadc05c0cd0bb7aa47b0648318d22a8ebad77e
qasulekha <qa@sulekha.com>
2025-12-05 18:17:41 +0530

    update

--- src/test/java/biz/promanage/tests/ActivityApprovalTest.java
+++ src/test/java/biz/promanage/tests/ActivityApprovalTest.java
@@ -21,7 +21,7 @@ public class ActivityApprovalTest extends BaseTest {
         new LoginPage(getDriver(), test).login(GetUserLoginCred());
         if (new Customer(getDriver(), test).changeCustomer("Sulekha.com")) {
         	 String APPROVED  ="Approved";
-        	 String CMO ="C.USER TEST";
+        	 String CMO ="New CMO";
         	 String POST ="Posts";
         	 ActivityApproval activityApprovalTest = new ActivityApproval(getDriver(), test);
         	 activityApprovalTest
