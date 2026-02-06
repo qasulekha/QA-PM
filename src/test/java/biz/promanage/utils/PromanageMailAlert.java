299de9cded5886ab00114a7fb9cc58961b182df3
qasulekha <qa@sulekha.com>
2026-02-06 17:36:14 +0530

    Update

--- src/test/java/biz/promanage/utils/PromanageMailAlert.java
+++ src/test/java/biz/promanage/utils/PromanageMailAlert.java
@@ -8,7 +8,7 @@ public class PromanageMailAlert {
     public static void SendMail(String subject, String body,String mailTo) {
 
         final String username = "qa@sulekha.com";
-        final String password = "Testing82024";
+        final String password = "Testing22026";
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
