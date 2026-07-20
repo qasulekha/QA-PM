c6f94f78671779ed153bd6cb469f0ce81b2d9c15
qasulekha <qa@sulekha.com>
2026-07-20 14:20:41 +0530

    Xpath change

--- src/test/java/biz/promanage/Pages/ActivityApproval.java
+++ src/test/java/biz/promanage/Pages/ActivityApproval.java
@@ -18,7 +18,7 @@ public class ActivityApproval extends BasePage {
     private final By ACTIVITY__TYPE = By.xpath("(//div[@class='sk-flex-row approval-dropdown sk-margin-bottom']/div)[1]");
     private final By ACTIVITY__TYPE_POST = By.xpath("((//div[@class='sk-flex-row approval-dropdown sk-margin-bottom']/div)[1]//child::li)[2]");
     private final By RESPONDED_BY  = By.xpath("(//div[@class='sk-event-group'])[4]");
-    private final By C_USER_TEST = By.xpath("(//div[@class='sk-overflow-list sk-show']/ul/li)[2]");
+    private final By C_USER_TEST = By.xpath("(//div[@class='sk-overflow-list sk-show']/ul/li)[1]");
     private final By RESPONDED_BY_CMO = By.xpath("(((//div[@class='sk-data-table'])[1]//tr)[2]//td)[6]//div");
     private final By STATUS = By.xpath("(//div[@class='sk-event-group'])[5]");
     private final By STATUS_APPROVAL = By.xpath("(//div[@class='sk-overflow-list sk-show']//child::li)[3]");
