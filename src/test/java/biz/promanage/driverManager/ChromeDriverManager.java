44429e5a709ef96ac6614f97a8a3aa35ce1c1ce0
qasulekha <qa@sulekha.com>
2026-08-11 10:31:23 +0530

    Version update

--- src/test/java/biz/promanage/driverManager/ChromeDriverManager.java
+++ src/test/java/biz/promanage/driverManager/ChromeDriverManager.java
@@ -20,7 +20,7 @@ public class ChromeDriverManager extends DriverManager {
 	protected void startDriver() throws IOException {
 	    ChromeOptions chromeOptions = new ChromeOptions();
 	    
-	    //chromeOptions.addArguments("--headless=new");
+	    chromeOptions.addArguments("--headless=new");
 	    chromeOptions.addArguments("--disable-gpu");
 	    chromeOptions.addArguments("--incognito");
 	    chromeOptions.addArguments("start-maximized");
